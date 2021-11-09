08493972e958020480a6888c028879511b50c911
Si1kn <si1kn521@gmail.com>
2021-11-09 11:24:36 +1000

    First commit

---
+++ src/main/java/io/github/si1kn/template/Template.java
@@ -0,0 +1,10 @@
+package io.github.si1kn.template;
+
+import io.github.si1kn.logger.Logger;
+
+public class Template {
+
+    public static void main(String[] args) {
+        Logger.getInstance().info("Hello World!");
+    }
+}
